bceb483d37a43a38ba79897680d2d8793b8f2483
Mariana Vieira <114255481+MarianaV11@users.noreply.github.com>
2023-01-09 02:47:24 -0300

    Refactoring code

--- C++ Course/Hangman.cpp
+++ C++ Course/Hangman.cpp
@@ -31,50 +31,64 @@ bool live () {
     return wrong_guess.size() < 5;
 }
 
-int main () {
+void header () {
     cout << "**************************" << endl;
     cout << "* Welcome to the Hangman *" << endl;
     cout << "**************************" << endl;
     cout << endl;
     cout << "Say your guess to the secret word!" << endl;
+}
 
-    cout << SECRET_WORD << endl;
+void print_wguesses () {
+    cout << "Wrong guesses: ";
+    for (char word : wrong_guess) {
+        cout << word << " ";
+    }
+    cout << endl;
+}
+
+void print_word () {
+    for (char word : SECRET_WORD) {
+        if (tried[word]) {
+        cout << word << "";
+        } else {
+             cout << "_ ";
+        }
+    }
+    cout << endl;
+}
+
+void tries () {
+    char guess;
+    cin >> guess;
+
+    tried[guess] = true;
+
+    if (letter_in(guess)) {
+        cout << "You are right! your guess is in the word." << endl;
+    }
+    else {
+        cout << "You are wrong! your guess isn't in the word." << endl;
+        wrong_guess.push_back(guess);
+    }
+    cout << endl;
+}
+
+int main () {
+    header();
 
     while (wrong() && live()) {
-        cout << "Wrong guesses: ";
-        for (char word : wrong_guess) {
-            cout << word << " ";
-        }
-        cout << endl;
-        
-        for (char word : SECRET_WORD) {
-            if (tried[word]) {
-            cout << word << "";
-            } else {
-                cout << "_ ";
-            }
-        }
-        cout << endl;
+        print_wguesses();
 
-        char guess;
-        cin >> guess;
+        print_word();
 
-        tried[guess] = true;
-
-        if (letter_in(guess)) {
-            cout << "You are right! your guess is in the word." << endl;
-        }
-        else {
-            cout << "You are wrong! your guess isn't in the word." << endl;
-            wrong_guess.push_back(guess);
-        }
-        cout << endl;
+        tries();
     }
 
     cout << "End Game!" << endl;
     cout << "The right word is " << SECRET_WORD << endl;
     if (wrong()) {
-        cout << "You lost, Try it again!" << endl;
+        cout << "You lost, try it again!" << endl;
     } else {
         cout << "Congratulations! You got the right word!" << endl;
     }
